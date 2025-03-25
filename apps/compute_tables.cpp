e67d5e4e4ca6e1ceb9b334649ebe67fe3fb0ae6f
carmeloevoli <carmelo.evoli@gssi.it>
2025-03-25 22:38:53 +0800

    table production list

--- apps/compute_tables.cpp
+++ apps/compute_tables.cpp
@@ -69,40 +69,40 @@ int main() {
     simprop::utils::startup_information();
 
     // proton losses
-    // auto pairLosses = GnuPropTables::protonLosses<simprop::losses::PairProductionLosses>();
-    // pairLosses->run("gnuprop_proton_losses_pair.bin");
-    // auto pionLosses = GnuPropTables::protonLosses<simprop::losses::PhotoPionContinuousLosses>();
-    // pionLosses->run("gnuprop_proton_losses_photopion.bin");
+    auto pairLosses = GnuPropTables::protonLosses<simprop::losses::PairProductionLosses>();
+    pairLosses->run("gnuprop_proton_losses_pair.bin");
+    auto pionLosses = GnuPropTables::protonLosses<simprop::losses::PhotoPionContinuousLosses>();
+    pionLosses->run("gnuprop_proton_losses_photopion.bin");
 
     // absorption rates
-    // auto gammaAbsCmb = GnuPropTables::absorptionCached<cache::GammaAbsorptionCached>(cmb);
-    // gammaAbsCmb->run("gnuprop_absorption_gammas_cmb.bin", 1e-3);
-    // auto pairAbsCmb = GnuPropTables::absorptionCached<cache::PairAbsorptionCached>(cmb);
-    // pairAbsCmb->run("gnuprop_absorption_pairs_cmb.bin", 1e-3);
-    // auto gammaAbsEbl = GnuPropTables::absorptionCached<cache::GammaAbsorptionCached>(ebl);
-    // gammaAbsEbl->run("gnuprop_absorption_gammas_ebl.bin", 1e-2);
-    // auto pairAbsEbl = GnuPropTables::absorptionCached<cache::PairAbsorptionCached>(ebl);
-    // pairAbsEbl->run("gnuprop_absorption_pairs_ebl.bin", 1e-2);
+    auto gammaAbsCmb = GnuPropTables::absorptionCached<cache::GammaAbsorptionCached>(cmb);
+    gammaAbsCmb->run("gnuprop_absorption_gammas_cmb.bin", 1e-3);
+    auto pairAbsCmb = GnuPropTables::absorptionCached<cache::PairAbsorptionCached>(cmb);
+    pairAbsCmb->run("gnuprop_absorption_pairs_cmb.bin", 1e-3);
+    auto gammaAbsEbl = GnuPropTables::absorptionCached<cache::GammaAbsorptionCached>(ebl);
+    gammaAbsEbl->run("gnuprop_absorption_gammas_ebl.bin", 1e-2);
+    auto pairAbsEbl = GnuPropTables::absorptionCached<cache::PairAbsorptionCached>(ebl);
+    pairAbsEbl->run("gnuprop_absorption_pairs_ebl.bin", 1e-2);
 
     // photo-pion rates
-    // auto ppNuCmb = GnuPropTables::photoPionCached<interactions::PhotoPionNeutrinos>(cmb);
-    // ppNuCmb->run("gnuprop_photopion_neutrinos_cmb.bin", 1e-3);
-    // auto ppPairCmb = GnuPropTables::photoPionCached<interactions::PhotoPionPairs>(cmb);
-    // ppPairCmb->run("gnuprop_photopion_pairs_cmb.bin", 1e-3);
-    // auto ppGammaCmb = GnuPropTables::photoPionCached<interactions::PhotoPionGammas>(cmb);
-    // ppGammaCmb->run("gnuprop_photopion_gammas_cmb.bin", 1e-3);
-    // auto ppNuEbl = GnuPropTables::photoPionCached<interactions::PhotoPionNeutrinos>(ebl);
-    // ppNuEbl->run("gnuprop_photopion_neutrinos_ebl.bin", 1e-2);
-    // auto ppGammaEbl = GnuPropTables::photoPionCached<interactions::PhotoPionGammas>(ebl);
-    // ppGammaEbl->run("gnuprop_photopion_gammas_ebl.bin", 1e-2);
-    // auto ppPairEbl = GnuPropTables::photoPionCached<interactions::PhotoPionPairs>(ebl);
-    // ppPairEbl->run("gnuprop_photopion_pairs_ebl.bin", 1e-2);
+    auto ppNuCmb = GnuPropTables::photoPionCached<interactions::PhotoPionNeutrinos>(cmb);
+    ppNuCmb->run("gnuprop_photopion_neutrinos_cmb.bin", 1e-3);
+    auto ppPairCmb = GnuPropTables::photoPionCached<interactions::PhotoPionPairs>(cmb);
+    ppPairCmb->run("gnuprop_photopion_pairs_cmb.bin", 1e-3);
+    auto ppGammaCmb = GnuPropTables::photoPionCached<interactions::PhotoPionGammas>(cmb);
+    ppGammaCmb->run("gnuprop_photopion_gammas_cmb.bin", 1e-3);
+    auto ppNuEbl = GnuPropTables::photoPionCached<interactions::PhotoPionNeutrinos>(ebl);
+    ppNuEbl->run("gnuprop_photopion_neutrinos_ebl.bin", 1e-2);
+    auto ppGammaEbl = GnuPropTables::photoPionCached<interactions::PhotoPionGammas>(ebl);
+    ppGammaEbl->run("gnuprop_photopion_gammas_ebl.bin", 1e-2);
+    auto ppPairEbl = GnuPropTables::photoPionCached<interactions::PhotoPionPairs>(ebl);
+    ppPairEbl->run("gnuprop_photopion_pairs_ebl.bin", 1e-2);
 
     // photo-pair rates
-    // auto ppCmb = GnuPropTables::photoPairCached(cmb);
-    // ppCmb->run("gnuprop_photopair_pairs_cmb.bin", 1e-3);
-    // auto ppEbl = GnuPropTables::photoPairCached(ebl);
-    // ppEbl->run("gnuprop_photopair_pairs_ebl.bin", 1e-2);
+    auto ppCmb = GnuPropTables::photoPairCached(cmb);
+    ppCmb->run("gnuprop_photopair_pairs_cmb.bin", 1e-3);
+    auto ppEbl = GnuPropTables::photoPairCached(ebl);
+    ppEbl->run("gnuprop_photopair_pairs_ebl.bin", 1e-2);
 
     // IC rates
     auto icCmb = GnuPropTables::inverseComptonCached(cmb);
@@ -119,89 +119,3 @@ int main() {
 
   return 0;
 }
-
-// void compute_photo_pair_rates() {
-//   simprop::utils::Timer timer("photo-pair production rate");
-
-//   cache::PhotoPairRate(ebl, redshifts, xAxis, energyAxis, "gnuprop_photopair_pairs_ebl.bin");
-//   cache::PhotoPairRate(cmb, redshifts, xAxis, energyAxis, "gnuprop_photopair_pairs_cmb.bin");
-// }
-
-// compute_photo_pion_rates();
-//   compute_photo_pair_rates();
-//   compute_IC_rates();
-
-// static std::unique_ptr<cache::PairAbsorptionCached> pairAbsorptionCmb() {
-//   auto ic = std::make_unique<cache::PairAbsorptionCached>();
-//   ic->buildEnergyAxis(1e15 * SI::eV, 1e22 * SI::eV, 1000);
-//   ic->buildRedshiftAxis(0., 6., 6);
-//   ic->buildPhotonField(cmb);
-//   return ic;
-// }
-
-// static std::unique_ptr<cache::PairAbsorptionCached> pairAbsorptionEbl() {
-//   auto ic = std::make_unique<cache::PairAbsorptionCached>();
-//   ic->buildEnergyAxis(1e15 * SI::eV, 1e22 * SI::eV, 1000);
-//   ic->buildRedshiftAxis(0., 6., 6);
-//   ic->buildPhotonField(ebl);
-//   return ic;
-// }
-
-// void compute_proton_losses() {
-//   simprop::utils::Timer timer("proton losses");
-
-//   const auto eProtonAxis = simprop::utils::LogAxis(1e16 * SI::eV, 1e24 * SI::eV, 8 * 128);
-
-//   cache::ProtonLosses<simprop::losses::PairProductionLosses>(cmb, eProtonAxis,
-//                                                              "gnuprop_proton_losses_pair.bin");
-//   cache::ProtonLosses<simprop::losses::PhotoPionContinuousLosses>(
-//       cmb, eProtonAxis, "gnuprop_proton_losses_photopion.bin");
-// }
-
-// void compute_photopair_absorption_rates() {
-//   simprop::utils::Timer timer("photo-pair absorption rates");
-
-//   cache::GammaAbsorptionRate(cmb, redshifts, energyAxis, "gnuprop_absorption_gammas_cmb.bin");
-//   cache::GammaAbsorptionRate(ebl, redshifts, energyAxis, "gnuprop_absorption_gammas_ebl.bin");
-// }
-
-// void compute_ic_absorption_rates() {
-//   simprop::utils::Timer timer("IC absorption rates");
-
-//   cache::PairAbsorptionRate(cmb, redshifts, energyAxis, "gnuprop_absorption_pairs_cmb.bin");
-//   cache::PairAbsorptionRate(ebl, redshifts, energyAxis, "gnuprop_absorption_pairs_ebl.bin");
-// }
-
-// void compute_photo_pion_rates() {
-//   simprop::utils::Timer timer("photo-pion production rate");
-
-//   cache::PhotoPionRate<interactions::PhotoPionNeutrinos>(ebl, redshifts, xAxis, energyAxis,
-//                                                          "gnuprop_photopion_neutrinos_ebl.bin");
-//   cache::PhotoPionRate<interactions::PhotoPionGammas>(ebl, redshifts, xAxis, energyAxis,
-//                                                       "gnuprop_photopion_gammas_ebl.bin");
-//   cache::PhotoPionRate<interactions::PhotoPionPairs>(ebl, redshifts, xAxis, energyAxis,
-//                                                      "gnuprop_photopion_pairs_ebl.bin");
-//   cache::PhotoPionRate<interactions::PhotoPionNeutrinos>(cmb, redshifts, xAxis, energyAxis,
-//                                                          "gnuprop_photopion_neutrinos_cmb.bin");
-//   cache::PhotoPionRate<interactions::PhotoPionGammas>(cmb, redshifts, xAxis, energyAxis,
-//                                                       "gnuprop_photopion_gammas_cmb.bin");
-//   cache::PhotoPionRate<interactions::PhotoPionPairs>(cmb, redshifts, xAxis, energyAxis,
-//                                                      "gnuprop_photopion_pairs_cmb.bin");
-// }
-
-// void compute_photo_pair_rates() {
-//   simprop::utils::Timer timer("photo-pair production rate");
-
-//   cache::PhotoPairRate(ebl, redshifts, xAxis, energyAxis, "gnuprop_photopair_pairs_ebl.bin");
-//   cache::PhotoPairRate(cmb, redshifts, xAxis, energyAxis, "gnuprop_photopair_pairs_cmb.bin");
-// }
-
-// void compute_IC_rates() {
-//   simprop::utils::Timer timer("IC production rate");
-
-//   cache::InverseComptonRate(cmb, redshifts, xAxis, energyAxis, "gnuprop_IC_gammas_cmb.bin");
-//   cache::InverseComptonRate(ebl, redshifts, xAxis, energyAxis, "gnuprop_IC_gammas_ebl.bin");
-//   cache::InverseComptonRate(cmb, redshifts, xAxis, energyAxis, "gnuprop_IC_pairs_cmb.bin",
-//   false); cache::InverseComptonRate(ebl, redshifts, xAxis, energyAxis,
-//   "gnuprop_IC_pairs_ebl.bin", false);
-// }
