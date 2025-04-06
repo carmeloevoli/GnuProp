790ad0a5ff07614dc93acaee33cbb1fd6c28ed8d
carmeloevoli <carmelo.evoli@gssi.it>
2025-04-06 07:29:25 +0200

    no IC and no EBL

--- apps/run.cpp
+++ apps/run.cpp
@@ -4,11 +4,13 @@
 class RunModels {
  public:
   struct ModelParameters {
-    double evolutionIndex;
-    double injectionSlope;
-    double cutoffEnergy;
-    double doCmbNeutrinos;
-    double doEblNeutrinos;
+    double evolutionIndex = 0.;
+    double zMax = 5.;
+    double redshiftSize = 1000000;
+    double injectionSlope = 2.5;
+    double cutoffEnergy = 0.;
+    bool doCmbNeutrinos = false;
+    bool doEblNeutrinos = false;
     std::string outputFilename;
   };
 
@@ -16,42 +18,60 @@ class RunModels {
     simprop::utils::Timer timer("Time for running model " + params.outputFilename);
 
     auto g = std::make_unique<gnuprop::GnuProp>(std::make_unique<simprop::cosmo::Cosmology>());
+    g->setRedshiftMax(params.zMax);
+    g->setRedshiftSize(params.redshiftSize);
     g->setEvolutionIndex(params.evolutionIndex);
     g->setInjectionSlope(params.injectionSlope);
     if (params.cutoffEnergy > 0) {
       g->setHeCutoff(params.cutoffEnergy);
     }
+
+    g->addProtonLossRate(
+        std::make_unique<gnuprop::ProtonLossRate>("tables/gnuprop_proton_losses_pair.bin"));
+    g->addProtonLossRate(
+        std::make_unique<gnuprop::ProtonLossRate>("tables/gnuprop_proton_losses_photopion.bin"));
+
     if (params.doCmbNeutrinos) {
       g->addPhotoPionNeutrinoSource(
           std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopion_neutrinos_cmb.bin"));
-
       g->addPhotoPionGammaSource(
           std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopion_gammas_cmb.bin"));
-      g->addInverseComptonGammaSource(std::make_unique<gnuprop::ProductionRate>(
-          "tables/gnuprop_inversecompton_gammas_cmb.bin"));
-
       g->addPhotoPionElectronSource(
           std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopion_pairs_cmb.bin"));
-      g->addInverseComptonElectronSource(
-          std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_inversecompton_pairs_cmb.bin"));
+
+      g->addGammaAbsorption(
+          std::make_unique<gnuprop::AbsorptionRate>("tables/gnuprop_absorption_gammas_cmb.bin"));
       g->addPhotoPairElectronSource(
           std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopair_pairs_cmb.bin"));
+
+      // g->addElectronAbsorption(
+      //     std::make_unique<gnuprop::AbsorptionRate>("tables/gnuprop_absorption_pairs_cmb.bin"));
+
+      // g->addInverseComptonGammaSource(std::make_unique<gnuprop::ProductionRate>(
+      //     "tables/gnuprop_inversecompton_gammas_cmb.bin"));
+
+      // g->addInverseComptonElectronSource(
+      //     std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_inversecompton_pairs_cmb.bin"));
     }
+
     if (params.doEblNeutrinos) {
-      g->addPhotoPionNeutrinoSource(
-          std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopion_neutrinos_ebl.bin"));
+      // g->addGammaAbsorption(
+      //     std::make_unique<gnuprop::AbsorptionRate>("tables/gnuprop_absorption_gammas_ebl.bin"));
 
-      g->addPhotoPionGammaSource(
-          std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopion_gammas_ebl.bin"));
-      g->addInverseComptonGammaSource(std::make_unique<gnuprop::ProductionRate>(
-          "tables/gnuprop_inversecompton_gammas_ebl.bin"));
+      // g->addPhotoPionNeutrinoSource(
+      //     std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopion_neutrinos_ebl.bin"));
 
-      g->addPhotoPionElectronSource(
-          std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopion_pairs_ebl.bin"));
-      g->addInverseComptonElectronSource(
-          std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_inversecompton_pairs_ebl.bin"));
-      g->addPhotoPairElectronSource(
-          std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopair_pairs_ebl.bin"));
+      // g->addPhotoPionGammaSource(
+      //     std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopion_gammas_ebl.bin"));
+      // g->addInverseComptonGammaSource(std::make_unique<gnuprop::ProductionRate>(
+      //     "tables/gnuprop_inversecompton_gammas_ebl.bin"));
+
+      // g->addPhotoPionElectronSource(
+      //     std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopion_pairs_ebl.bin"));
+      // g->addPhotoPairElectronSource(
+      //     std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_photopair_pairs_ebl.bin"));
+      // g->addInverseComptonElectronSource(
+      //     std::make_unique<gnuprop::ProductionRate>("tables/gnuprop_inversecompton_pairs_ebl.bin"));
     }
     g->build();
     g->evolve(0.);
@@ -79,8 +99,8 @@ int main() {
       params.evolutionIndex = 3.;
       params.injectionSlope = 2.5;
       params.cutoffEnergy = 1e23 * SI::eV;
-      params.doCmbNeutrinos = 1;
-      params.doEblNeutrinos = 0;
+      params.doCmbNeutrinos = true;
+      params.doEblNeutrinos = false;
       params.outputFilename = "gnuprop_spectrum_dipmodel_m3_zmax5_cmb.txt";
       RunModels::runModel(params);
     }
