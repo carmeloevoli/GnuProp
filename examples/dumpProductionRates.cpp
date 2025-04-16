50d366c91df5dba75c34e4fb0b054ca92f77262d
Carmelo Evoli <carmelo.evoli@gssi.it>
2025-04-16 16:11:15 +0200

    minor changes

--- examples/dumpProductionRates.cpp
+++ examples/dumpProductionRates.cpp
@@ -47,16 +47,16 @@ int main() {
 
     dumpRates("gnuprop_photopair_pairs_cmb");
     dumpRates("gnuprop_photopair_pairs_ebl");
-    dumpRates("gnuprop_inversecompton_gammas_cmb");
-    dumpRates("gnuprop_inversecompton_gammas_ebl");
-    dumpRates("gnuprop_inversecompton_pairs_cmb");
-    dumpRates("gnuprop_inversecompton_pairs_ebl");
     dumpRates("gnuprop_photopion_neutrinos_cmb");
     dumpRates("gnuprop_photopion_neutrinos_ebl");
     dumpRates("gnuprop_photopion_gammas_cmb");
     dumpRates("gnuprop_photopion_gammas_ebl");
     dumpRates("gnuprop_photopion_pairs_cmb");
     dumpRates("gnuprop_photopion_pairs_ebl");
+    dumpRates("gnuprop_inversecompton_gammas_cmb");
+    dumpRates("gnuprop_inversecompton_gammas_ebl");
+    dumpRates("gnuprop_inversecompton_pairs_cmb");
+    dumpRates("gnuprop_inversecompton_pairs_ebl");
 
     dumpRates2D("gnuprop_photopair_pairs_cmb");
     dumpRates2D("gnuprop_photopair_pairs_ebl");
